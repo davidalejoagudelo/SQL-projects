8e782bd09f8342a5f765d6c8a9c3c8d582952d63
David Alejandro Agudelo Castillo <davidalejoagudelo@gmail.com>
2025-01-17 11:43:40 -0500

    Update 10. Ejercicios.sql

--- Clases/10. Eventos triggers/10. Ejercicios.sql
+++ Clases/10. Eventos triggers/10. Ejercicios.sql
@@ -1,3 +1,9 @@
-/* Ejercicios Triggers */
+/* Ejercicios TRIGGERS
 
-/* 1. Crear un trigger que se ejecute cuando se realice un pago para abonar la deuda de Honduland */
+1. Crear un Trigger que se ejecute cuando
+
+	Realices un pago para abonar la deuda del cliente Honduland en la venta registrada con
+    2400. Corroborar que el Trigger se haya ejecutado correctamente al cargar la vista de 
+    clientes morosos
+
+*/
